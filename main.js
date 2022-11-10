2f0c2315a602ac77083511ce2c45a4a3b224c0a5
easybarezi20 <easybarezi20@gmail.com>
2022-11-10 13:23:40 -0800

    Hunger number incrementing changed function types

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const hungerNum = document.querySelector('.hungerNumber')
+const hungerNum = document.querySelector('#hungerNumber')
 
 
 function headerIncrease() {
-
+    let num = parseInt(hungerNum.innerHTML)
+    num += 1
+    hungerNum.innerHTML = num
 }
+setInterval(headerIncrease, 1000)
