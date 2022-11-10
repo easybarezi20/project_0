b1d02e95994efb28f4307320dbee991634e78ca9
easybarezi20 <easybarezi20@gmail.com>
2022-11-10 13:46:21 -0800

    hunger timer stops at 10 and feed button resets to zero while timer continues

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const hungerNum = document.querySelector('#hungerNumber')
+const hungerBtn = document.querySelector('#hungerbutton')
 
 let hungerIncrease = setInterval(() => {
+    if(hungerNum.innerHTML < 10){
     let num = parseInt(hungerNum.innerHTML)
     num += 1
     hungerNum.innerHTML = num
-    if (hungerNum.innerHTML == 10){
+    }else if (hungerNum.innerHTML === 10){
         clearInterval(hungerIncrease)
+        
     }
 }, 1000)
 
-const hungerBtn = document.querySelector('#hungerbutton')
+
+
+
 console.log(hungerBtn)
+hungerBtn.addEventListener('click', () => {
+    hungerNum.innerHTML = 0
+})
