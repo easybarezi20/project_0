a803e6f0ec67ed4a86877b06fe2a4ed0a13c8cec
easybarezi20 <easybarezi20@gmail.com>
2022-11-10 16:48:56 -0800

    added death text but number wont increment for hunger

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const hungerNum = document.querySelector('#hungerNumber')
 const hungerBtn = document.querySelector('#hungerbutton')
+let first = document.querySelector('#first')
+let last = document.querySelector('#last')
+
 
 let hungerIncrease = setInterval(() => {
     if(hungerNum.innerHTML < 10){
     let num = parseInt(hungerNum.innerHTML)
     num += 1
     hungerNum.innerHTML = num
-    }else if (hungerNum.innerHTML === 10){
+    }else if (hungerNum.innerHTML === 10){ 
+        first.textContent = ''
+        last.textContent = 'you died!'
         clearInterval(hungerIncrease)
-        
     }
 }, 1000)
 
@@ -24,11 +28,15 @@ let sleepinessIncrease = setInterval(() => {
     let num = parseInt(sleepNum.innerHTML)
     num += 1
     sleepNum.innerHTML = num
-    }else if (sleepNum.innerHTML === 10){
+    }else if (sleepNum.innerHTML === '10'){
+        let first = document.querySelector('#first')
+        let last = document.querySelector('#last')
+        first.textContent = ''
+        last.textContent = 'you died!'
         clearInterval(hungerIncrease)
         
     }
-}, 3000)
+}, 1000)
 
 sleepBtn.addEventListener('click', () => {
     sleepNum.innerHTML = 0
@@ -42,7 +50,11 @@ let boredomIncrease = setInterval(() => {
     let num = parseInt(boredomNum.innerHTML)
     num += 1
     boredomNum.innerHTML = num
-    }else if (boredomNum.innerHTML === 10){
+    }else if (boredomNum.innerHTML === '10'){
+        let first = document.querySelector('#first')
+        let last = document.querySelector('#last')
+        first.textContent = ''
+        last.textContent = 'you died!'
         clearInterval(hungerIncrease)
         
     }
@@ -70,3 +82,14 @@ form.addEventListener("submit", (event) => {
     finalName.innerHTML = input.value
     input.value = ''
 })
+
+// function death() {
+//     if(hungerNum.innerHTML === 10){
+//         let first = document.querySelector('#first')
+//          let last = document.querySelector('#last')
+//          first.textContent = ''
+//          last.textContent = 'you died!'
+//     }
+// }
+// death()
+// console.log(typeof hungerNum)
